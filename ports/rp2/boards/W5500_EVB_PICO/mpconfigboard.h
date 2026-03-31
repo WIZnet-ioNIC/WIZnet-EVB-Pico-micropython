12e12134fed2cc9a13096600f3a5e7d80d5a090f
wiznet-grace <Grace@wiznet.io>
2026-03-31 15:42:42 +0900

    extmod/network_wiznet5k,6k: Add socket timeout and setsockopt support.
    Signed-off-by: wiznet-grace <Grace@wiznet.io>

--- ports/rp2/boards/W5500_EVB_PICO/mpconfigboard.h
+++ ports/rp2/boards/W5500_EVB_PICO/mpconfigboard.h
@@ -6,6 +6,7 @@
 // Enable networking.
 #define MICROPY_PY_NETWORK                  (1)
 #define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "W5500-EVB"
+#define MICROPY_PY_SOCKET_EXTENDED_STATE    (1)
 
 // Wiznet HW config.
 #define MICROPY_HW_WIZNET_SPI_ID            (0)
